9b3ad915ed2687054fe6650f143f6d37643e19c8
Miles Wells <mileswells93@gmail.com>
2021-11-13 09:52:40 -0500

    init

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+registerPlugin({
+	name: 'refurbish-manager',
+	version: '1.0',
+	authors: ['MilesWells'],
+	type: 'remote',
+	licence: 'ISC',
+	targetApiVersion: 34,
+	main: () => {
+		console.log('hi');
+		console.log('hot');
+	},
+});
